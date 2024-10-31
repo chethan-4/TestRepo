37c0b0a028f05ae545cb133b2d53594fb863c83e
chethan-4 <gowdamchethan4@gmail.com>
2024-10-31 12:43:15 +0530

    First commit

---
+++ src/nested_if/Java.java
@@ -0,0 +1,18 @@
+package nested_if;
+
+public class Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a=10, b=20, c=30;
+if (a<b)
+{
+	if (c>b)
+	System.out.println("c is greater than a and b");
+		
+}
+else 
+	System.out.println("a is not greater than b");
+	}
+
+}
